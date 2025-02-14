d3d5547fcadd4b704db43cd960c90523ccbab31c
Martin Rodriguez <neomartinr@gmail.com>
2025-02-14 20:33:42 -0300

    Widget clima 'terminado'

--- js/weather.js
+++ js/weather.js
@@ -2,12 +2,49 @@ const KEY = "b08332c778fcad97cc7aa0a56e974241";
 
 const baseURL = `https://api.openweathermap.org/data/2.5/weather?appid=${KEY}&units=metric`;
 
+let latitud = 35.671174;
+let longitud = 139.774497;
+
 const weatherInput = document.querySelector("#weather-search");
 
+const weatherBtnSave = document.getElementById("weather-save"); 
 
-(function obtenerClimaPorCoordenadas() {
+let ultimaCiudadValida;
 
-    axios.get(`${baseURL}&lat=35.671174&lon=139.774497`)   // Japón 35.671174, 139.774497
+const ciudadGuardada = localStorage.getItem("clima-ciudad");
+
+if(ciudadGuardada) {
+
+    obtenerClimaPorNombreCiudad(ciudadGuardada);
+
+} else {
+
+    if(navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+
+                const coords = position.coords;
+                latitud = coords.latitude;
+                longitud = coords.longitude;
+                obtenerClimaPorCoordenadas(latitud, longitud);
+
+            }, // successCallback
+            (error) => {
+                console.log("Error al obtener la ubicación");
+                obtenerClimaPorCoordenadas(latitud, longitud);
+            } // errorCallback
+        )
+
+    }
+
+    
+}
+
+
+function obtenerClimaPorCoordenadas(LATI, LONG) {
+
+    axios.get(`${baseURL}&lat=${LATI}&lon=${LONG}`)   // Japón 35.671174, 139.774497
         .then(response => {
             console.log(response.data);
             const weather = response.data;
@@ -18,11 +55,10 @@ const weatherInput = document.querySelector("#weather-search");
             console.log(errorsito);	
         })
 
-})(); // IIFE para invocar inmediatamente la función
+} // IIFE para invocar inmediatamente la función
 
 
 weatherInput.addEventListener("keyup", function(event) {
-
     if(event.key === "Enter") {
         
         const valorInput = event.target.value.toLowerCase();
@@ -30,18 +66,66 @@ weatherInput.addEventListener("keyup", function(event) {
         obtenerClimaPorNombreCiudad(valorInput);
 
     } 
-
 })
 
-function obtenerClimaPorNombreCiudad(ciudad) {
+// weatherInput.addEventListener("input", function(event) {
+//     weatherBtnSave.disabled = true;
+// })
+
+weatherBtnSave.addEventListener("click", function() {
+
+    // const ciudad = weatherInput.value.toLowerCase();
+
+    localStorage.setItem("clima-ciudad", ultimaCiudadValida);
+
+    Swal.fire({
+        title: "Ciudad guardada",
+        text: `Se guardó la ciudad de ${ultimaCiudadValida}`,
+        icon: "info"
+    })
+
+    // obtenerClimaPorNombreCiudad(ciudad, 123);
+});
+
+
+
+
+
+
+
+
+function obtenerClimaPorNombreCiudad(ciudad, save = false) {
     
     axios.get(`${baseURL}&q=${ciudad}`)
                 .then(response => {
                     const weather = response.data;
                     pintarClima(weather);
+
+                    weatherBtnSave.disabled = false;
+                    // weatherBtnSave.classList.remove("disabled");
+
+                    ultimaCiudadValida = ciudad;
+
+
+                    Swal.fire({
+                        title: "Ciudad encontrada",
+                        text: `Se encontró la ciudad de ${ciudad}`,
+                        icon: "success"
+                    })
+
+                    // if(save) {
+                    //     // Guardar el nombre de la ciudad en el localStorage
+                    //     localStorage.setItem("clima-ciudad", ciudad);
+                    // }
                 })
                 .catch(error => {
                     console.log(error);
+                    weatherBtnSave.disabled = true;
+                    Swal.fire({
+                        title: "Ciudad no encontrada",
+                        text: `No se encontró la ciudad de ${ciudad}`,
+                        icon: "error"
+                    })
                 })
 
 }
